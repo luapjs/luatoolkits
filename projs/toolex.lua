80354e79a962106d3f26113276f6bad2f954ad23
canyon <gongyanghui1986@163.com>
2018-07-16 14:28:08 +0800

    添加 快速排序 sortArrayByField

--- projs/toolex.lua
+++ projs/toolex.lua
@@ -13,6 +13,8 @@ local _fmtColor = "<color=#%s>%s</color>";
 local table = table
 local table_insert = table.insert
 local table_sort = table.sort
+local table_keys = table.keys
+local table_lens = table.lens
 
 local string_format = string.format
 local string_gsub = string.gsub
@@ -62,7 +64,7 @@ function printTable( tb,title,notSort,rgb )
 			table_insert( str, "{" )
 			tabNum = tabNum + 1
 
-			local keys = table.keys(t);
+			local keys = table_keys(t);
 			if not notSort then table_sort(keys,_sort_key); end
 
 			local v,kk,ktp,vtp;
@@ -126,4 +128,132 @@ function readonly( tb )
 	}
 	setmetatable(_ret,_mt);
 	return _ret;
+end
+
+------ 排序相关 -----
+local function _quickSortBase(p)
+	if p == nil or p.h >= p.t then return end
+
+	local head,tail
+	head = p.h
+	tail = p.t
+	local key = p.ka[head]
+	local left,right
+	
+	left,right = head,tail
+	
+	while left < right do
+		while (left <right) and p.f(p.a[p.ka[right]],p.a[key]) >= 0 do
+			right = right - 1
+		end
+		p.ka[left] = p.ka[right]
+		
+	
+		while (left < right) and p.f(p.a[p.ka[left]] ,p.a[key]) < 0 do
+			left = left + 1
+		end
+		p.ka[right] = p.ka[left]
+	end
+	p.ka[left] = key
+
+
+	p.h = head
+	p.t = left - 1
+	_quickSortBase(p)
+	p.h = left + 1
+	p.t = tail
+	_quickSortBase(p)
+end 
+
+local function _quickSort( a, head, tail, f )
+	if head >= tail then
+		return
+	end
+
+	local key = a[head]
+	local left,right
+	
+	left,right = head,tail
+	
+	while left < right do
+		while (left <right) and f(a[right],key) >= 0 do
+			right = right - 1
+		end
+		a[left] = a[right]
+		
+	
+		while (left < right) and f(a[left] ,key) < 0 do
+			left = left + 1
+		end
+		a[right] = a[left]
+	end
+	a[left] = key
+
+	_quickSort( a, head, left - 1, f )
+	_quickSort( a, left + 1,tail, f )
+end
+
+local function _quickSort2(a,f)
+	local ka = {}
+	for k, v in pairs( a ) do
+		table_insert( ka, k )
+	end
+	local p = {}
+	p.a = a
+	p.ka = ka
+	p.h = 1
+	p.t = table_lens(ka)
+	p.f = f
+	_quickSortBase(p)
+	return ka
+end
+
+-- fields 是 array中对象tab的key值,如果对象前面加 "-" 标识排倒叙
+function sortArrayByField( array, fields )
+	-- 重载，允许只有一个字符串
+	if type( fields ) == "string" then
+		fields = { fields }
+	end
+
+	-- 处理一次fields
+	local fieldConfig = {}
+	for _, v in pairs( fields ) do
+		if string.sub( v, 1, 1 ) == "-" then
+			table_insert( fieldConfig, { string.sub( v, 2, string.len( v ) ), true } )
+		else
+			table_insert( fieldConfig, { v, false } )
+		end
+	end
+
+	-- 按照优先级进行排序
+	local sorter = function( a, b )
+		local ret = 0
+
+		for _, v in pairs( fieldConfig ) do
+			local field, desc = v[1], v[2]
+
+			local v1, v2 = a[field], b[field]
+			if v1 then
+				if desc then
+					ret = v2 - v1
+				else
+					ret = v1- v2
+				end
+
+				if ret ~= 0 then
+					return ret
+				end
+			end
+		end
+		return ret
+	end
+
+	local sortd = {}
+	local keys = _quickSort2( array, sorter )
+
+	for _, v in pairs( keys ) do
+		table_insert( sortd, array[ v ] )
+	end
+
+	return sortd
 end
