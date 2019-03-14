8eec970f380870ca6d622167918b3ac9a71dcdb3
canyon <gongyanghui1986@163.com>
2019-03-14 15:22:26 +0800

    抽出公共函数工具对象

---
+++ projs/toolkit.lua
@@ -0,0 +1,9 @@
+--- 工具
+-- Anthor : canyon / 龚阳辉
+-- Date : 2018-05-18 10：25
+-- Desc : 重新整理一遍
+
+require("projs/toolex");
+
+TB_EMPTY = {}; -- 全局空的对象(用于返回)
+TB_NEW = {__call=function() return {}; end};
